820f7990e6d8033ec03968312bce7548f951ad2b
dustin-py <dustinstri92@gmail.com>
2021-02-07 11:33:28 -0800

    updated port scanner

--- Scanners/utils/port_scanner.py
+++ Scanners/utils/port_scanner.py
@@ -33,12 +33,22 @@ class Porty:
 
     def basic_port_scan(self):
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(1)
-            result = s.connect_ex((self.host, int(self.port)))
-            if result == 0:
-                print("Port {} is open".format(self.port))
-            s.close()
+            if self.port == '-':
+                print("{}:".format(self.host))
+                for p in range(1,65535):
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    socket.setdefaulttimeout(1)
+                    result = s.connect_ex((self.host, p))
+                    if result == 0:
+                        print("Port {} is open".format(p))
+                    s.close()
+            else:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.setdefaulttimeout(1)
+                result = s.connect_ex((self.host, int(self.port)))
+                if result == 0:
+                    print("Port {} is open".format(self.port))
+                s.close()
         except socket.error:
             print("Couldn't connect to server.")
             sys.exit()
@@ -47,31 +57,42 @@ class Porty:
         host = self.make_host_list()
         port = self.make_port_list()
         try:
-            if type(host) == list and type(port) != list:
-                for addr in host:
+            if len(host) > 1 and len(port) == 1:
+                if self.port == '-':
+                    for addr in host:
+                        print("{}:".format(addr))
+                        for p in range(1,65535):
+                            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            socket.setdefaulttimeout(1)
+                            result = s.connect_ex((addr, p))
+                            if result == 0:
+                                print("Port {} is open".format(p))
+                            s.close()
+                else:
+                    for addr in host:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        socket.setdefaulttimeout(1)
+                        result = s.connect_ex((addr,int(port)))
+                        print("{}:".format(addr))
+                        if result == 0:
+                            print("Port {} is open".format(port))
+                        s.close()
+            elif len(port) > 1 and len(host) == 1:
+                print("{}:".format(host[0]))
+                for p in port:
                     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     socket.setdefaulttimeout(1)
-                    result = s.connect_ex((addr,int(port)))
-                    print("{}:".format(addr))
-                    if result == 0:
-                        print("Port {} is open".format(port))
-                    s.close()
-            elif type(port) == list and type(host) != list:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                socket.setdefaulttimeout(1)
-                for p in port:
-                    result = s.connect_ex((host,int(p)))
-                    print("{}:".format(addr))
+                    result = s.connect_ex((host[0],int(p)))
                     if result == 0:
                         print("Port {} is open".format(p))
                     s.close()
-            else:
+            else: 
                 for addr in host:
-                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    socket.setdefaulttimeout(1)
+                    print("{}:".format(addr))
                     for p in port:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        socket.setdefaulttimeout(1)
                         result = s.connect_ex((addr,int(p)))
-                        print("{}:".format(addr))
                         if result == 0:
                             print("Port {} is open".format(p))
                         s.close()
